eaf58f5dd9e64a0513274bd2b9d1d234bef63d3d
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-04-27 19:12:40 +0200

    update task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- script that lists all bands with "Glam rock" as their main style,
 -- ranked by their longevity
 
-SELECT band_name, (IFNULL(split, 2022) - formed) as lifespan
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock';
+WHERE style LIKE '%Glam rock%';
