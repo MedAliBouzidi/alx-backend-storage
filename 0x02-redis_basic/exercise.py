d906b1dfd595cfa6cd64d377b1219be6cedd15e8
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-05-01 15:51:02 +0200

    fix task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,14 +1,16 @@
 #!/usr/nin/env python3
 """ Cache class """
 from typing import Union
-from redis import Redis
 from uuid import uuid4
+
+from redis import Redis
 
 
 class Cache:
     def __init__(self):
         """Constructor"""
-        self._redis = Redis()
+        self._redis = Redis(host="localhost", port=6379, db=0)
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in redis"""
